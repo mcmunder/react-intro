27b9c7b769a8f8e2566f0925a0aae88c208ce62d
Matthias Christoph Munder <matthiasmunder@gmail.com>
2017-01-06 17:16:10 +0100

    Changes

--- app/src/components/EventInspector/EventInspector.js
+++ app/src/components/EventInspector/EventInspector.js
@@ -9,11 +9,27 @@ class EventInspector extends Component {
     }
   }
 
+  handleEvent (e) {
+    this.setState({
+      currentEvent: e.type
+    })
+  }
   render () {
     return (
-      <div className='event-container'>
+      <div
+        className='event-container'
+        onClick={(e) => this.handleEvent(e)}
+        onDoubleClick={(e) => this.handleEvent(e)}
+        onChange={(e) => this.handleEvent(e)}
+        onTouchStart={(e) => this.handleEvent(e)}
+        onTouchEnd={(e) => this.handleEvent(e)}
+        onCopy={(e) => this.handleEvent(e)}
+        onPaste={(e) => this.handleEvent(e)}
+        onMouseEnter={(e) => this.handleEvent(e)}
+        onMouseLeave={(e) => this.handleEvent(e)} >
         <div className='event-inspector'>
-          <h4>Current Event</h4>
+          <h3>Event Inspector</h3>
+          <h4>Current Event:</h4>
           <h4>{this.state.currentEvent}</h4>
         </div>
       </div>
